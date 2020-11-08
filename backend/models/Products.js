493da9110f08cea2cbb4fffa68399b93bba15cc0
Ana Tcaci <anachiscov@gmail.com>
2020-11-08 12:24:36 +0100

    product model

--- backend/models/Products.js
+++ backend/models/Products.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-    name : String,
-    category: String,
-    price: Number,
-    imgURL: String
+    name : {
+        type:String,
+        required: true,
+        },
+    category: {
+        type:String,
+        required: true,
+        }, 
+    price: {
+        type:Number,
+        required: true,
+        default: 0
+        }, 
+    imgURL: {
+        type:String,
+        required: true,
+        },
+    InStock: {
+        type:Number,
+        required: true,
+        default: 0
+    }
  },{
      timestamps: true
  }
