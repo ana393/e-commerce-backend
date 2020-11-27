2c471cd0a1e859ff226ef0555d547092c9546a46
Ana Tcaci <anachiscov@gmail.com>
2020-11-27 16:50:34 +0100

    implement functions for uploading process

--- backend/app.js
+++ backend/app.js
@@ -31,6 +31,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/uploads',express.static('uploads'));
 app.use(express.static(path.join(__dirname, "public")));
 //validate the session
 app.use(session({
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -4,11 +4,27 @@ const ProductsController= require('../controllers/ProductController') ;
 const passport = require('passport');
 const { only } = require('../config/utils.js');
 const multer =  require('multer');
-const upload = multer({dest: 'uploads/'});
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+const filter = (re, file, cb) => {
+    if (file.mimetype ==='image/jpeg' || file.mimetype === 'image/png'){
+       cb(null, true);
+    } else {
+         //don´t save the file
+         cb(new Error('Unfullfilled type requirements'), false);
+    }  
+}
+const upload = multer({storage: storage, limits:{fileSize: 1024 * 1024 * 5}, fileFilter: filter});
 
 
-router.post('/insert', passport.authenticate('jwt', { session:false}),only(['admin']), ProductsController.insertProduct);
-router.put('/update', upload.single('prodImg'), ProductsController.updateProduct);
+router.post('/insert',passport.authenticate('jwt', { session:false}),only(['admin']), upload.single('imgURL'),  ProductsController.insertProduct);
+router.put('/update', ProductsController.updateProduct);
 router.delete('/:id',passport.authenticate('jwt', { session:false}),only(['admin']),  ProductsController.deleteProduct);
 //for all users
 router.get('/search', ProductsController.filterProduct)
