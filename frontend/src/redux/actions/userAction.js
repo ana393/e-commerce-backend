db31be002427ae886e951d80fc22f7b993732c89
Ana Tcaci <anachiscov@gmail.com>
2020-12-05 17:03:31 +0100

    login action function/checked with backend

--- frontend/src/redux/actions/userAction.js
+++ frontend/src/redux/actions/userAction.js
@@ -4,12 +4,21 @@ import { API_URL } from '../../config'
 import { userActions } from '../actions/types';
 
 export const signup = async(user) => {
-    const res =   await axios.post(API_URL + '/users/signup', user);
-    console.log('signup',res.data)
+    const res =   await axios.post(API_URL + '/users/signUp', user);
+    console.log('signUp',res.data)
     store.dispatch({
         type: userActions.SIGNUP,
         payload: res.data.user
     })
-    
+   
+}
+export const login = async(user) => {
+    const res =   await axios.post(API_URL + '/users/login', user);
+    console.log('login',res.data);
+    localStorage.setItem('authToken', res.data.token);
+    store.dispatch({
+        type: userActions.SIGNIN,
+        payload: res.data.user
+    }) 
     
 }
